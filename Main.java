84b034d70eefae58758e9c88239a7e1a79db6917
AngeloCS2C <116428273+AngeloCS2C@users.noreply.github.com>
2023-01-19 00:48:08 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car("Toyota", "Camry", 4);
+        Motorcycle motorcycle = new Motorcycle("PEDRO_PENDUKO", "Road King", false);
+        Fleet fleet = new Fleet();
+        fleet.addVeh
